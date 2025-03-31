1b7d64eec0ecd047f3f454f31df62d5677b19e82
Måns Thörnvik <thornvik.mans@gmail.com>
2025-03-31 22:49:43 +0200

    update go app

--- sample-go-app/cmd/main.go
+++ sample-go-app/cmd/main.go
@@ -1,9 +1,15 @@
 package main
 
-import "log"
+import (
+	"log"
 
+	"github.com/maansaake/github-actions-base/sample-go-app/internal/somepkg"
+)
+
+//nolint:gochecknoglobals // .
 var Version = "unset"
 
 func main() {
 	log.Println("Hello, World!", "Version="+Version)
+	log.Println("Calling util: ", somepkg.Util())
 }
